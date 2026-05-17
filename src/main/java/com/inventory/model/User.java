d09a1ee52c21cb7019a8277811f5d3b18dcbd701
SachinthaMadugalla <sachinthanipun04@gmail.com>
2026-05-17 09:45:45 +0530

    updated user

--- src/main/java/com/inventory/model/User.java
+++ src/main/java/com/inventory/model/User.java
@@ -17,20 +17,30 @@ public class User {
     }
 
     //Getters & Setters
-    public String getUsername()              { return username; }
-    public void   setUsername(String u)      { this.username = u; }
+    public String getUsername() {
+        return username;
+    }
+    public void   setUsername(String u) {
+        this.username = u;
+    }
 
-    public String getPassword()              { return password; }
-    public void   setPassword(String p)      { this.password = p; }
+    public String getPassword() {
+        return password;
+    }
+    public void   setPassword(String p) {
+        this.password = p;
+    }
 
-    public String getRole()                  { return role; }
-    public void   setRole(String r)          { this.role = r; }
-
+    public String getRole() {
+        return role;
+    }
+    public void   setRole(String r) {
+        this.role = r;
+    }
 
     public String toCsv() {
         return username + "," + password + "," + role;
     }
-
 
     public static User fromCsv(String csv) {
         String[] parts = csv.split(",", -1);
